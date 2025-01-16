234a05ff410568d11dd03fd7c6b8be117799048c
sahilrush <sahilrevankar20co21@gmail.com>
2025-01-16 16:04:18 +0530

    buy logic

--- README.md
+++
@@ -1,1 +0,0 @@
-# opnify-go
--- src/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type UserResponse struct {
-	Message string      `json:"message"`
-	Success bool        `json:"success"`
-	Data    interface{} `json:"data,omitempty"`
-}
